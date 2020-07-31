dba659dda866235dba3d90fd1d4a06838fa184ee
citizenz7 <citizenz7@protonmail.com>
2020-07-31 10:04:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ACS-Blog-Template
---
+++ single.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying article page
+ *
+ * @link Github
+ *
+ * @package WordPress
+ * @subpackage Acs-Blog-Template
+ * @since Acs-Blog-Template 1.0
+ * @version 1.0
+ */
