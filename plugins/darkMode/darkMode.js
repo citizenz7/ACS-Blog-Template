dfabe7eb112335e9a71112a63eab531880bcfe3b
RDemarch <robin.demarch@sfr.fr>
2020-07-31 11:40:20 +0200

    Supression de junk code

--- plugins/darkMode/darkMode.js
+++ plugins/darkMode/darkMode.js
@@ -1,6 +1,4 @@
 var body = document.getElementsByTagName("BODY");
-var div = document.getElementsByTagName("DIV");
-var p = document.getElementsByTagName("P");
 var newButton = document.createElement("button")
 document.body.appendChild(newButton);
 newButton.setAttribute("onclick", "darkMode()")
@@ -8,30 +6,10 @@ newButton.classList.add("btn-darkMode")
 
 darkMode = function() {
 
-
-
   if (!body[0].classList.contains("darkMode")) {
-
-    for (i = 0; i < body.length; i++) {
-      body[i].classList.add("darkMode");
-    }
-    for (i = 0; i < p.length; i++) {
-      p[i].classList.add("darkMode");
-    }
-    for (i = 0; i < div.length; i++) {
-      div[i].classList.add("darkMode");
-    }
+      body[0].classList.add("darkMode");
   }
   else if (body[0].classList.contains("darkMode")) {
-
-    for (i = 0; i < p.length; i++) {
-      p[i].classList.remove("darkMode");
-    }
-    for (i = 0; i < body.length; i++) {
-      body[i].classList.remove("darkMode");
-    }
-    for (i = 0; i < div.length; i++) {
-      div[i].classList.remove("darkMode");
-    }
+      body[0].classList.remove("darkMode");
   }
 }
